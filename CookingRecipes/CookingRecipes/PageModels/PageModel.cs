2801cbcc57b2e8028386e693f8b7534a27f4c92c
Alex <alexander.t.null@outlook.com>
2021-09-28 20:42:51 +0700

    Базовая модель для PageModels

---
+++ CookingRecipes/CookingRecipes/PageModels/PageModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CookingRecipes.PageModels
+{
+    /// <summary>Базовый класс для PageModels.</summary>
+    public class PageModel : INotifyPropertyChanged
+    {
+        /// <inheritdoc />
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>Уведомляет об изменении свойства.</summary>
+        /// <param name="propertyName">Имя свойства.</param>
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>Устанавливает новое значение свойству.</summary>
+        /// <typeparam name="T">Тип свойства.</typeparam>
+        /// <param name="field">Ссылка на поле, которое скрывает свойство.</param>
+        /// <param name="value">Новое значение.</param>
+        /// <param name="propertyName">Имя свойства.</param>
+        /// <returns><see langword="true"/>, если значение свойства поменялось; иначе <see langword="false"/>.</returns>
+        protected virtual bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (field != null && Equals(field, value))
+            {
+                return false;
+            }
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
