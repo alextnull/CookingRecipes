1bee6b735b0d8a04cbfd6a26a6a6e3797b12e934
Alex <alexander.t.null@outlook.com>
2021-10-05 11:52:29 +0700

    Детали рецепта без похожих рецептов

---
+++ CookingRecipes/CookingRecipes/PageModels/RecipeDetailPageModel.cs
@@ -0,0 +1,32 @@
+﻿using CookingRecipes.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CookingRecipes.PageModels
+{
+    public class RecipeDetailPageModel : PageModel
+    {
+        /// <summary>Рецепт.</summary>
+        private Recipe _recipe;
+
+        /// <summary>Провайдер для навигации.</summary>
+        private INavigation _navigationProvider;
+
+        public Recipe Recipe
+        {
+            get => _recipe;
+            set => SetProperty(ref _recipe, value);
+        }
+
+        /// <summary>Инициализирует PageModel деталей рецепта.</summary>
+        /// <param name="recipe">Рецепт.</param>
+        /// <param name="navigationProvider">Провайдер для навигации.</param>
+        public RecipeDetailPageModel(Recipe recipe, INavigation navigationProvider)
+        {
+            _navigationProvider = navigationProvider;
+            Recipe = recipe;
+        }
+    }
+}
---
+++ CookingRecipes/CookingRecipes/Pages/RecipeDetailPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using CookingRecipes.PageModels;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CookingRecipes.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RecipeDetailPage : ContentPage
+    {
+        public RecipeDetailPage(RecipeDetailPageModel recipeDetailPageModel)
+        {
+            InitializeComponent();
+            BindingContext = recipeDetailPageModel;
+        }
+    }
+}
